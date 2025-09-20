eb14dd3c90b0e065ddc10d82b844f517533bc0b1
Aryan-saini1 <sainiaryan59@gmail.com>
2025-09-20 19:17:28 +0530

    organised files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// --- FIX IS HERE ---
 import Login from './components/Login.jsx';
 import Signup from './components/Signup.jsx';
-// --------------------
+import ForgotPassword from './components/ForgotPassword.jsx'; // 1. Import the new component
 import './App.css';
 
 function App() {
@@ -12,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} /> {/* 2. Add the new route */}
         {/* Redirect to login page by default */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
