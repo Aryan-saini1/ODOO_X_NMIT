841bae5a1194ba4327de3f0436893487f348cd6d
Weasley18 <harshringsia23cy@rnsit.ac.in>
2025-09-21 07:20:04 +0530

    merge graphQL and postgres

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -3,8 +3,19 @@ import ProductBom from './components/ProductBom'
 import Mo from './components/Mo'
 import Inventory from './components/Inventory'
 import Wo from './components/Wo'
+import DatabaseData from './components/DatabaseData'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    // In a real app, you'd get this from a login flow
+    const demoToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJkZW1vLWFkbWluIiwicm9sZXMiOlsiYWRtaW4iXSwiaWF0IjoxNjYyNTk1MjAwfQ.YOUR_SIGNATURE'
+    setToken(demoToken)
+  }, [])
+
   return (
     <>
       <h1>ODOO X NMIT - API Test UI</h1>
@@ -13,6 +24,7 @@ function App() {
         <Mo />
         <Inventory />
         <Wo />
+        <DatabaseData token={token} />
       </div>
     </>
   )
